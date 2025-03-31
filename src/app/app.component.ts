1b196ba10072177dab7c4b98cf77eb6dffdb9385
Alejandro de Olózaga Ramírez <deolozaga2004@gmail.com>
2025-03-31 23:42:29 +0100

    add: main page (index) -> app.component.html edit: changed some styles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from './components/header/header.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {IlustratedLinkComponent} from './components/ilustrated-link/ilustrated-link.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, IlustratedLinkComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
