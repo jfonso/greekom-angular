c6aa6008fdc852b6393bad17f56191e7bf6757e4
Alejandro de Olózaga Ramírez <deolozaga2004@gmail.com>
2025-04-01 17:39:06 +0100

    add: sign-up page and navbar component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import {ArticleComponent} from './pages/article/article.component';
 import {IndexComponent} from './pages/index/index.component';
 import {LogInComponent} from './pages/log-in/log-in.component';
+import {SignUpComponent} from './pages/sign-up/sign-up.component';
 
 export const routes: Routes = [
-  {path: 'index', component: IndexComponent},
+  {path: '', component: IndexComponent},
   {path: 'article', component: ArticleComponent},
   {path: 'log-in', component: LogInComponent},
+  {path: 'sign-up', component: SignUpComponent},
 ];
