17ef9c0e16e317ac7540d362b1ad906212ae00f1
Alejandro de Olózaga Ramírez <deolozaga2004@gmail.com>
2025-04-21 19:48:08 +0100

    add: firebase connection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "greekom-a009b", appId: "1:138065545618:web:3d37b218f355a107bd994b", storageBucket: "greekom-a009b.firebasestorage.app", apiKey: "AIzaSyB-JWwRQcHvkBivcHBmiaPt3wt8D1YIeDs", authDomain: "greekom-a009b.firebaseapp.com", messagingSenderId: "138065545618" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())]
 };
