ead6affd319d88dfa62bda0e23dff091654ee256
Alejandro de Olózaga Ramírez <deolozaga2004@gmail.com>
2025-04-22 18:50:37 +0100

    started: responsive design. Added header button for mobiles.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,26 @@ import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './components/header/header.component';
 import {FooterComponent} from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import {HeaderMobileComponent} from './components/header-mobile/header-mobile.component';
+
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, NavbarComponent, FooterComponent, HeaderMobileComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'greekom-angular';
+
+  isMobile = false;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver.observe([Breakpoints.HandsetPortrait])
+      .subscribe(result => {
+        this.isMobile = result.matches;
+      });
+  }
 }
