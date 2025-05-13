36b85217eb8659dba794749489dc32ae2857366e
Javier A. Alfonso Quintana <javier.alfonso101@alu.ulpgc.es>
2025-05-13 13:44:52 +0100

    Ionic configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,8 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
+import { provideIonicAngular } from '@ionic/angular/standalone';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "greekom-a009b", appId: "1:138065545618:web:3d37b218f355a107bd994b", storageBucket: "greekom-a009b.firebasestorage.app", apiKey: "AIzaSyB-JWwRQcHvkBivcHBmiaPt3wt8D1YIeDs", authDomain: "greekom-a009b.firebaseapp.com", messagingSenderId: "138065545618" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "greekom-a009b", appId: "1:138065545618:web:3d37b218f355a107bd994b", storageBucket: "greekom-a009b.firebasestorage.app", apiKey: "AIzaSyB-JWwRQcHvkBivcHBmiaPt3wt8D1YIeDs", authDomain: "greekom-a009b.firebaseapp.com", messagingSenderId: "138065545618" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage()), provideIonicAngular({})]
 };
