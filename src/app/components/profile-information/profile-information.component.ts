fab5acc987e5669838a291a07dcd5043078ed10d
Alejandro de Olózaga Ramírez <deolozaga2004@gmail.com>
2025-05-20 18:07:53 +0100

    edit: fixed some responsive styles and changed profile styles.

--- src/app/components/profile-information/profile-information.component.ts
+++ src/app/components/profile-information/profile-information.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import {InfoLabelComponent} from '../info-label/info-label.component';
+import {ProfileFavouriteItemComponent} from '../profile-favourite-item/profile-favourite-item.component';
+import {ProfileChangePasswordComponent} from '../profile-change-password/profile-change-password.component';
 
 @Component({
   selector: 'app-profile-information',
   imports: [
-    InfoLabelComponent
+    InfoLabelComponent,
+    ProfileFavouriteItemComponent,
+    ProfileChangePasswordComponent
   ],
   templateUrl: './profile-information.component.html',
   styleUrl: './profile-information.component.scss'
